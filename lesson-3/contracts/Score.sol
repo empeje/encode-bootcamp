5f063cc321d0939ab7c65f25b272b98666716917
empeje <11813607+empeje@users.noreply.github.com>
2022-04-19 21:19:39 +0700

    Make getter view function

--- lesson-3/contracts/Score.sol
+++ lesson-3/contracts/Score.sol
@@ -1,9 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
+
 pragma solidity ^0.8.0;
 
 contract Score {
-    uint score = 5;
+    uint public score = 5;
 
-    function getScore() public returns (uint) {
+    function getScore() public view returns (uint) {
         return score;
     }
 
