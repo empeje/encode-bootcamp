c29ba51041b0556bc9f2f50fd91e552e3f988835
empeje <11813607+empeje@users.noreply.github.com>
2022-04-19 21:53:51 +0700

    Implement event

--- lesson-3/contracts/Score.sol
+++ lesson-3/contracts/Score.sol
@@ -4,12 +4,36 @@ pragma solidity ^0.8.0;
 
 contract Score {
     uint public score = 5;
+    address owner;
+
+    event newScoreEvent(uint indexed);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        if (msg.sender == owner) {
+            _;
+        }
+    }
+
+    modifier Fee(uint fee) {
+        if (msg.value >= fee) {
+            _;
+        }
+    }
 
     function getScore() public view returns (uint) {
         return score;
     }
 
-    function setScore(uint new_score) public{
+    function forceSetScore(uint new_score) public payable Fee(100000) {
+        score = new_score;
+        emit newScoreEvent(score);
+    }
+
+    function setScore(uint new_score) public onlyOwner {
         score = new_score;
     }
 }
