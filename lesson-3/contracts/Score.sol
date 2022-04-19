97965ce9ac550d27c42aca3de0810e70241e6355
empeje <11813607+empeje@users.noreply.github.com>
2022-04-19 21:58:00 +0700

    Implement struct example

--- lesson-3/contracts/Score.sol
+++ lesson-3/contracts/Score.sol
@@ -2,15 +2,25 @@
 
 pragma solidity ^0.8.0;
 
+struct Funder {
+    address addr;
+    uint amount;
+}
+
 contract Score {
     uint public score = 5;
     address owner;
     mapping (address => uint) public score_list;
+    uint my_score = score_list[owner];
+    Funder giver;
+
 
     event newScoreEvent(uint indexed);
 
     constructor() {
         owner = msg.sender;
+        giver.addr = address (0xBA7283457B0A138890F21DE2ebCF1AfB9186A2EF);
+        giver.amount = 2500;
     }
 
     modifier onlyOwner {
