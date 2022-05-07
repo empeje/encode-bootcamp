d8554fe9a4f2d2d33a96c51a5bfd22eaec240656
empeje <11813607+empeje@users.noreply.github.com>
2022-05-07 19:41:57 +0700

    Implement volcano coin

--- lesson-4/contracts/VolcanoCoin.sol
+++ lesson-4/contracts/VolcanoCoin.sol
@@ -3,5 +3,44 @@
 pragma solidity ^0.8.0;
 
 contract VolcanoCoin {
+    uint256 totalSupply =  10000;
+    address owner;
+    mapping (address => uint256) balances;
+    struct Payment {
+        uint256 transferAmount;
+        address to;
+        address from;
+    }
 
+    Payment[] payments;
+
+    event totalSupplyChanged(uint256);
+
+    constructor() {
+        owner = msg.sender;
+        balances[msg.sender] = totalSupply;
+    }
+
+    modifier onlyOwner {
+        if (msg.sender == owner) {
+            _;
+        }
+    }
+
+    function getTotalSupply() public onlyOwner view returns(uint256) {
+        return totalSupply;
+    }
+
+    function increaseTotalSupply() public onlyOwner{
+        totalSupply += 1000;
+        emit totalSupplyChanged(totalSupply);
+    }
+
+    function transfer(uint256 amount, address to) public {
+        require(balances[msg.sender] > amount, "Insufficient balance");
+        balances[msg.sender] = balances[msg.sender] - amount;
+        balances[to] = balances[to] + amount;
+
+        payments.push(Payment({transferAmount: amount, to: to, from: msg.sender}));
+    }
 }
